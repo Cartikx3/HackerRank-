18621d3a0fa3ded238fb909b75c3bfab4a800fd9
Kartik Srivardhan <kartikshrivardhan@gmail.com>
2021-07-14 11:35:11 +0530

    Create solution.py

---
+++ Arithmetic Operators/solution.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    x=a+b
+    print (x)
+
+    y=a-b
+    
+    if a>b:
+        print (y)
+    else :
+        print (y)
+    z= a*b
+    print (z)
