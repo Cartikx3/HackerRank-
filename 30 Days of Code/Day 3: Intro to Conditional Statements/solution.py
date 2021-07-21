7cef9571abed1c09a9e59689b95c4edaf327022a
Kartik Srivardhan <kartikshrivardhan@gmail.com>
2021-07-21 09:30:09 +0530

    Create solution.py

---
+++ 30 Days of Code/Day 3: Intro to Conditional Statements/solution.py
@@ -0,0 +1,20 @@
+import math
+import os
+import random
+import re
+import sys
+
+N = int(input().strip())
+
+if N % 2 != 0:
+    print ("Weird")
+    
+elif 2<N<5 :
+    print ("Not Weird")
+    
+elif 6<N<=20  :
+    print ("Weird")
+    
+elif N>20:
+    print ("Not Weird")
+    
