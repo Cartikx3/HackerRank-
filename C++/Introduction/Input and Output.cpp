1ed6b5d76b2d95ae56f4736c15f0a96068a8cdf7
Kartik Srivardhan <kartikshrivardhan@gmail.com>
2021-08-06 19:53:46 +0530

    Create Input and Output.cpp

---
+++ C++/Introduction/Input and Output.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a , b , c , z ;
+    cin >> a >> b >> c;
+    z = a + b + c ;
+    cout << z << endl;
+    return 0;
+}
